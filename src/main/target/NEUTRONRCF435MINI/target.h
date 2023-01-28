d5244fa336894e2a5b63c5769e679218f3221c2c
EMSR <shanggl@wo.cn>
2023-01-28 13:49:41 +0800

    add NEUTRONFCF435MINI QFN48 target

--- src/main/target/NEUTRONRCF435MINI/target.h
+++ src/main/target/NEUTRONRCF435MINI/target.h
@@ -17,9 +17,9 @@
 
 #pragma once
 
-#define TARGET_BOARD_IDENTIFIER "NTAA"
+#define TARGET_BOARD_IDENTIFIER "NTAM"
 
-#define USBD_PRODUCT_STRING  "NEUTRONRCF435AIO"
+#define USBD_PRODUCT_STRING  "NEUTRONRCF435MINI"
 
 /**********swd debuger reserved *****************
  *
@@ -39,8 +39,8 @@
  * PH1 HEXT OUT
  */
   
-#define LED0                    PC4
-//#define LED1                    PD14
+#define LED0                    PC13
+#define LED1                    PC14
 
 #define BEEPER                  PC15
 #define BEEPER_INVERTED
@@ -52,10 +52,10 @@
 #define SPI1_SCK_PIN            PA5
 #define SPI1_MISO_PIN           PA6
 #define SPI1_MOSI_PIN           PA7
-#define SPI1_NSS_PIN            PA15
+#define SPI1_NSS_PIN            PA4
 
 #define USE_EXTI 
-#define GYRO_INT_EXTI           PA8
+#define GYRO_INT_EXTI           PA15
 #define USE_MPU_DATA_READY_SIGNAL
 
 // MPU6500
@@ -87,8 +87,8 @@
 // *************** I2C/Baro/Mag/EXT*********************
 #define USE_I2C
 #define USE_I2C_DEVICE_2
-#define I2C2_SCL                PB10        // SCL pad
-#define I2C2_SDA                PB11        // SDA pad
+#define I2C2_SCL                PH2        // SCL pad
+#define I2C2_SDA                PH3        // SDA pad
 #define USE_I2C_PULLUP
 
 #define USE_BARO
@@ -112,10 +112,10 @@
  
 // *************** OSD *****************************
 #define USE_SPI_DEVICE_2 
-#define SPI2_SCK_PIN            PB13//PB13 on LQFP64
-#define SPI2_MISO_PIN           PB14//PB14 on LQFP64
-#define SPI2_MOSI_PIN           PB15//PB15 on LQFP64
-#define SPI2_NSS_PIN            PB12 //confirm on lqfp64
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
+#define SPI2_NSS_PIN            PB12
 
 #define USE_MAX7456
 #define MAX7456_SPI_BUS         BUS_SPI2
@@ -123,60 +123,56 @@
 
 
 // *************** SD/BLACKBOX **************************
-#define USE_SPI_DEVICE_3
-#define SPI3_SCK_PIN            PB3
-#define SPI3_MISO_PIN   	    PB4
-#define SPI3_MOSI_PIN   	    PB5
-#define SPI3_NSS_PIN 			PB9 //confirm on lqfp64
-  
+ 
 #define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
 #define USE_FLASHFS
 #define USE_FLASH_M25P16
-#define M25P16_SPI_BUS          BUS_SPI3
-#define M25P16_CS_PIN           SPI3_NSS_PIN
+#define M25P16_SPI_BUS          BUS_SPI2
+#define M25P16_CS_PIN           PB5
 
 #define USE_FLASH_W25N01G
 #define W25N01G_SPI_BUS         BUS_SPI3
-#define W25N01G_CS_PIN          SPI3_NSS_PIN
+#define W25N01G_CS_PIN          PB5
 
 // *************** UART *****************************
 #define USE_VCP
 //#define USB_DETECT_PIN          PC14
 #define USE_USB_DETECT
 
-#define USE_UART1
-#define UART1_RX_PIN            PA10
-#define UART1_TX_PIN            PA9
+// REDEFINE for servo pwm output  
+// #define USE_UART1
+// #define UART1_RX_PIN            PB7
+// #define UART1_TX_PIN            PA9
 
-#define USE_UART2
-#define UART2_RX_PIN            PA3
-#define UART2_TX_PIN            PA2
+// #define USE_UART2
+// #define UART2_RX_PIN            PB0
+// #define UART2_TX_PIN            PA2
 
 #define USE_UART3
 #define UART3_RX_PIN            PB11
 #define UART3_TX_PIN            PB10
 
-#define USE_UART4
-#define UART4_RX_PIN            PC11
-#define UART4_TX_PIN            PC10
+#define USE_UART5
+#define UART5_RX_PIN            PB8
+#define UART5_TX_PIN            PB9
 
-#define USE_UART8
-#define UART8_RX_PIN            PD2
-#define UART8_TX_PIN            PC12
+#define USE_UART7
+#define UART7_RX_PIN            PB3
+#define UART7_TX_PIN            PB4
 
-#define SERIAL_PORT_COUNT       6
+#define SERIAL_PORT_COUNT       3
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_CRSF
-#define SERIALRX_UART           SERIAL_PORT_USART2
+#define SERIALRX_UART           SERIAL_PORT_USART7
 
 // *************** ADC *****************************
 #define USE_ADC
 #define ADC_INSTANCE                ADC1
 // todo
 #define ADC1_DMA_STREAM             DMA2_CHANNEL5
-#define ADC_CHANNEL_1_PIN           PC2
-#define ADC_CHANNEL_2_PIN           PC1
+#define ADC_CHANNEL_1_PIN           PA0
+#define ADC_CHANNEL_2_PIN           PA1
 //#define ADC_CHANNEL_3_PIN           PB0
 #define VBAT_ADC_CHANNEL            ADC_CHN_1
 #define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
@@ -198,6 +194,7 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         0xffff
 #define TARGET_IO_PORTE         BIT(2)
+#define TARGET_IO_PORTH         BIT(1)|BIT(2)|BIT(3)
 
 #define MAX_PWM_OUTPUT_PORTS        8
 #define USE_DSHOT
